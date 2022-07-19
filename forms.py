af13007d641b8714e44fd93d1ae0795245bf6f36
gabrielfernando01 <fernando.rozepeda@gmail.com>
2022-07-19 11:26:40 -0500

    update forms.py

--- forms.py
+++ forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequiered, Email, Lenght
+from wtforms import StringField, SubmitField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(FlaskForm):
     name=StringField('Nombre', validators=[DataRequired(), Length(max=64)])
     password=PasswordField('Password', validators=[DataRequired()])
     email=StringField('Email', validators=[DataRequired(), Email()])
-    submit=submitField('Registrar')
+    submit=SubmitField('Registrar')
+
+class PostForm(FlaskForm):
+    title = StringField('Título', validators=[DataRequired(), Length(max=128)])
+    title_slug = StringField('Título slug', validators=[Length(max=128)])
+    content = TextAreaField('Contenido')
+    submit = SubmitField('Enviar')
